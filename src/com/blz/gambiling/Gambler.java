3a4982a19309116e843bf073ff7df83a15871471
Manishakahar <manisha.kahar1998@gmail.com>
2021-12-01 00:37:01 +0530

    Added: Initialize Stake and Bet

--- src/com/blz/gambiling/Gambler.java
+++ src/com/blz/gambiling/Gambler.java
@@ -3,5 +3,8 @@ package com.blz.gambiling;
 public class Gambler {
     public static void main(String[] args) {
         System.out.println("Welcome To Gambling Simulation");
+        // Initializing Variables
+        final int STAKE_PER_DAY = 100;
+        final int BET = 1;
     }
 }
