970dc20a9275378471560832b2d24f705334bf4f
Manishakahar <manisha.kahar1998@gmail.com>
2021-12-02 06:03:18 +0530

    Added: Condition to check Gambler win or loose upto DailyLimit

--- src/com/blz/gambiling/Gambler.java
+++ src/com/blz/gambiling/Gambler.java
@@ -8,17 +8,23 @@ public class Gambler {
    public static final int WIN = 1;
    public static final int LOOSE = 0;
 
-     public static void dailyBet(){
+      // Function to Check Win Or Loss
+     public static void dailyBet() {
+         int min_Percent = STAKE_PER_DAY - (STAKE_PER_DAY * 50 / 100);
+         int mxm_Percent = STAKE_PER_DAY + (STAKE_PER_DAY * 50 / 100);
          int cash = STAKE_PER_DAY;
-         int option = (int) (Math.random() * 10 % 2) ;
-         if (option == WIN ){
-             cash += BET;
-             System.out.println("Player has won " +cash);
-         }else if(option ==LOOSE ){
-             cash -= BET;
+         int option = (int) (Math.random() * 10 % 2);
+         while (cash > min_Percent && cash < mxm_Percent) {
+             if (option == WIN) {
+                 cash += BET;
+                 System.out.println("Player has won " + cash);
+             } else if (option == LOOSE) {
+                 cash -= BET;
 
-             System.out.println("Player has loose " +cash);
+                 System.out.println("Player has loose " + cash);
+             }
          }
+         System.out.println("Player has left with Total amount  " +cash);
      }
     public static void main(String[] args) {
         System.out.println("Welcome To Gambling Simulation");
